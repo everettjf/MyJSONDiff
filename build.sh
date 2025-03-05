81d118525f5cea38c29a936a3b2e52ca2ef9d7b6
everettjf <everettjf@live.com>
2025-03-05 01:20:01 -0800

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 
 
+APPNAME="MyJSONDiff"
 # rustup target add x86_64-apple-darwin 
 
 
 npm run tauri build -- --bundles app --target universal-apple-darwin
+
+# security find-identity -v -p codesigning
+# security find-identity -v
+
+rm "$APPNAME.pkg"
+xcrun productbuild --sign "3rd Party Mac Developer Installer: Feng Zhu (SU4WK7V467)" --component "src-tauri/target/universal-apple-darwin/release/bundle/macos/$APPNAME.app" /Applications "$APPNAME.pkg"
